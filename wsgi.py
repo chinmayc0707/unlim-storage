f08e01586a44d74f4f9c54a775d9736e6481c2d4
Chinmay C Bhat <chinmayc200@gmail.com>
2026-04-13 11:20:09 +0530

    fix: update database URI scheme to postgresql and add dynamic port configuration for WSGI server

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,8 @@
+import os
 from app import app
 from waitress import serve
 
 if __name__ == "__main__":
-    print("Starting server on http://0.0.0.0:8080")
-    serve(app, host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    print(f"Starting server on http://0.0.0.0:{port}")
+    serve(app, host="0.0.0.0", port=port)
